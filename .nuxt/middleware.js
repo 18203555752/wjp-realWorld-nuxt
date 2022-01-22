5a7a2c310d0281932c18f4855db1db0ff6ce39dc
18203555752 <519688153@qq.com>
2022-01-22 11:57:05 +0800

    去掉.nuxt

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['hasUser'] = require('..\\middleware\\hasUser.js')
-middleware['hasUser'] = middleware['hasUser'].default || middleware['hasUser']
-
-middleware['noUser'] = require('..\\middleware\\noUser.js')
-middleware['noUser'] = middleware['noUser'].default || middleware['noUser']
-
-export default middleware
